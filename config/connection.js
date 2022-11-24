151edd0c988dde790a062457ef0827a2e67dacab
Abigail Douglas <EburgAbby@aol.com>
2022-11-24 04:31:05 -0800

    trying to fix env error

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 const Sequelize = require('sequelize');
+
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, './.env') })
+
+// const ck = require('ckey');
+
+// const userName = ck.USER;     // sample@gmail.com
+// const password = ck.PASSWORD; // iampassword123
+// const apiKey   = ck.API_KEY;  // 1234567890
+
+// require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is working
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
